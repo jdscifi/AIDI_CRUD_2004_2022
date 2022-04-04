8c30a4482eb719e3b083d63f5d72103fecf443c7
jdscifi <jayduttjoshi@gmail.com>
2022-04-04 02:15:09 -0400

    Implement "read" API
    https://github.com/jdscifi/AIDI_CRUD_2004_2022/projects/1#card-80087139

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, Response
 import json
 import sqlite3
 import datetime as dt
+import copy
+
 app = Flask(__name__)
 
 DATABASE = "database/student_records.db"
@@ -22,31 +24,58 @@ def create_record():
     if expected_keys != received_keys:
         a = received_keys - expected_keys
         if len(a) > 0:
-            return app.response_class(json.dumps({"error_message": "Unexpected key(s): {}".format(", ".join(list(a)))}), status=400, mimetype='application/json')
+            return app.response_class(json.dumps({"error_message": "Unexpected key(s): {}".format(", ".join(list(a)))}),
+                                      status=400, mimetype='application/json')
         b = expected_keys - received_keys
         if len(b) > 0:
-            return app.response_class(json.dumps({"error_message": "Missing key(s): {}".format(", ".join(list(b)))}), status=400, mimetype='application/json')
-    """for key, value in DATA_SKELETON.items():
-        if not isinstance(request_data[key], value):
-            
-            return app.response_class(json.dumps({"error_message": "{} expects {} received {}".format(key, value, type(request_data[key]))}), status=400, mimetype='application/json')"""
-    request_data['amount_due'] = float(request_data['amount_due'])
+            return app.response_class(json.dumps({"error_message": "Missing key(s): {}".format(", ".join(list(b)))}),
+                                      status=400, mimetype='application/json')
+
+    try:
+        request_data['amount_due'] = float(request_data['amount_due'])
+    except:
+        return app.response_class(response=json.dumps({"error_message": "Invalid Value For 'amount_due'"}), status=344,
+                                  mimetype='application/json')
     try:
         checking_date = dt.datetime.strptime(request_data['dob'], "%Y-%m-%d")
     except:
-        return app.response_class(response=json.dumps({"error_message": "dob expects YYYY-MM-DD format"}), status=344, mimetype='application/json')
+        return app.response_class(response=json.dumps({"error_message": "dob expects YYYY-MM-DD format"}), status=344,
+                                  mimetype='application/json')
     try:
         with sqlite3.connect(DATABASE, isolation_level='EXCLUSIVE', timeout=10) as connection:
             db = connection.cursor()
-            db.execute("""INSERT INTO 'student_info' ('first_name','last_name','dob','amount_due') VALUES('{}','{}','{}',{});""".format(request_data['first_name'], request_data['last_name'], request_data['dob'], request_data['amount_due']))
-        return app.response_class(response=json.dumps({"message": "New Record Created"}), status=201, mimetype='application/json')
+            db.execute(
+                """INSERT INTO 'student_info' ('first_name','last_name','dob','amount_due') VALUES('{}','{}','{}',{});""".format(
+                    request_data['first_name'], request_data['last_name'], request_data['dob'],
+                    request_data['amount_due']))
+        return app.response_class(response=json.dumps({"message": "New Record Created"}), status=201,
+                                  mimetype='application/json')
     except Exception as e:
-        return app.response_class(response=json.dumps({"error_message": "Error Creating New Record", "execption": str(e)}), status=344, mimetype='application/json')
+        return app.response_class(
+            response=json.dumps({"error_message": "Error Creating New Record", "execption": str(e)}), status=344,
+            mimetype='application/json')
 
 
-@app.route('/read', methods=["GET"])
+@app.route('/read', methods=["GET", "POST"])
 def read_record():
-    return app.response_class(response=json.dumps({"test": "hgbjhlsdgj"}), status=200, mimetype='application/json')
+    if request.method == "POST" or request.method == "GET":
+        try:
+            student_id = int(request.args.get("student_id"))
+        except KeyError:
+            return app.response_class(json.dumps({"error_message": "Student ID not provided"}), status=400, mimetype='application/json')
+        except ValueError:
+            return app.response_class(json.dumps({"error_message": "Invalid Student ID"}), status=400, mimetype='application/json')
+    try:
+        with sqlite3.connect(DATABASE) as connection:
+            connection.row_factory = sqlite3.Row
+            db = connection.cursor()
+            db.execute("""SELECT * FROM 'student_info' where student_id={};""".format(student_id))
+            query_output = db.fetchone()
+            if len(query_output) == 0:
+                return app.response_class(response=json.dumps({"message": "No Record Found"}), status=200, mimetype='application/json')
+        return app.response_class(response=json.dumps({"record": dict(query_output)}), status=200, mimetype='application/json')
+    except Exception as e:
+        return app.response_class(response=json.dumps({"error_message": "Error Retrieving Record", "execption": str(e)}), status=500, mimetype='application/json')
 
 
 @app.route('/update', methods=["PUT"])
